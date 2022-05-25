e0c76dfaabffbdc9da28dba0681ece6e33e2a6d4
wowanamatsu <antonioflavionte@gmail.com>
2022-05-25 12:16:41 -0300

    Movimentação do player

--- Woods/scripts/player.gd
+++ Woods/scripts/player.gd
@@ -1,5 +1,36 @@
 extends KinematicBody2D
 
+var velocity: Vector2
+export (int) var Speed = 60
 
-func _ready():
-	pass
+onready var animation: AnimationPlayer = get_node("Animation")
+onready var sprite: Sprite = get_node("Sprite")
+
+func _physics_process(_delta: float) -> void:
+	move()
+	verify_direction()
+	animate()
+	
+
+func move() -> void:
+	var direction: Vector2 = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("top")
+	).normalized()
+
+	velocity = direction * Speed
+	velocity = move_and_slide( velocity )
+
+
+func animate() -> void:
+	if velocity != Vector2.ZERO:
+		animation.play("run")
+	else:
+		animation.play("Idle")
+
+
+func verify_direction() -> void:
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
